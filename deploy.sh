ac7b69e45a4348cf683888b39f43089e12b8a2d1
Torben Carstens <github@carstens.tech>
2021-05-30 12:27:22 +0200

    Add gha

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ k8sfile=.kubernetes/manifest.yaml
 
 docker build -t $image:$v .
 docker push $image:$v
-sed -i -e "s/:{{tag}}/:${v}/g" $k8sfile
+sed -i -e "s/:{{TAG}}/:${v}/g" $k8sfile
 kubectl apply -f $k8sfile
-sed -i -e "s/:${v}/:{{tag}}/g" $k8sfile
+sed -i -e "s/:${v}/:{{TAG}}/g" $k8sfile
